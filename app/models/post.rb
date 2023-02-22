83f3e9f6ea430e39ef2001b2c393fdd8dc52405b
Rashid Menhas <rashidmenhas10@gmail.com>
2023-02-22 16:46:35 +0500

    Fix linter error.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,8 @@ class Post < ApplicationRecord
   after_save :update_posts_counter
 
   validates :title, presence: true, length: { in: 1..250 }
-  validates :comments_counter, :likes_counter, comparison: { greater_than_or_equal_to: 0 }, numericality: { only_integer: true }
+  validates :comments_counter, :likes_counter, comparison: { greater_than_or_equal_to: 0 },
+                                               numericality: { only_integer: true }
 
   belongs_to :author, class_name: 'User'
   has_many :comments, dependent: :destroy
