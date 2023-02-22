a1636baa0d9fd4ed50e16edb97f697cb802c6ac3
Rashid Menhas <rashidmenhas10@gmail.com>
2023-02-22 16:40:29 +0500

    Set validation for user comment counter and like counter is greater than or equal to 0

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'author_id', dependent: :destroy
   has_many :likes, foreign_key: 'author_id', dependent: :destroy
 
+  validates :name, presence: true
+  validates :posts_counter, comparison: { greater_than_or_equal_to: 0 }, numericality: { only_integer: true }
+
   def recent_three
     posts.order(created_at: :desc).limit(3)
   end
