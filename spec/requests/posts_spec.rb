12bf4367ab174c5c7c15fc17c63a39fb17ceef29
Rashid Menhas <rashidmenhas10@gmail.com>
2023-02-23 10:54:09 +0500

    Add spec in post spec file and add tests.

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,16 +2,38 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
+    before :each do
+      get '/users/:user_id/posts/'
+    end
+
     it 'returns http success' do
-      get '/posts/index'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'should render index' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'should include correct placeholder' do
+      expect(response.body).to include("Here are ALL Posts for a given user you are in '/users/user_id/posts'")
     end
   end
 
   describe 'GET /show' do
+    before :each do
+      get '/users/:user_id/posts/:id'
+    end
+
     it 'returns http success' do
-      get '/posts/show'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'should render show' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'should include correct placeholder' do
+      expect(response.body).to include("Here is a single Post for a given user you are in '/users/user_id/posts'")
     end
   end
 end
