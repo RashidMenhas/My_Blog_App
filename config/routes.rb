f5b1ae5c3da78c5705b22a95369e5c67900a9e08
Rashid Menhas <rashidmenhas10@gmail.com>
2023-02-25 12:33:24 +0500

    Update routes.rb file,

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,13 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "users#index"
+
+  get '/posts/new', to: 'posts#new', as: 'create_post'
+  post '/posts/new', to: 'posts#create'
+  get '/users/:user_id/posts/:id', to: 'posts#show', as:'post'
+  post '/users/:user_id/posts/:id', to: 'posts#create_comment'
+  post '/users/:user_id/posts/:id/likes', to: 'posts#create_like', as: 'like'
+
   resources :users, only: [:index, :show] do 
     resources :posts, only: [:index, :show]
     
