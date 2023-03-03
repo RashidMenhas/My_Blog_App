22786225c53f4ae04414ed66dd1e98847f4f0b5c
Rashid Menhas <rashidmenhas10@gmail.com>
2023-03-03 21:10:30 +0500

    Fix rubocop error.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         # I already include the above two line in my project
+  # I already include the above two line in my project
   has_many :posts, foreign_key: 'author_id', dependent: :destroy
   has_many :comments, foreign_key: 'author_id', dependent: :destroy
   has_many :likes, foreign_key: 'author_id', dependent: :destroy
