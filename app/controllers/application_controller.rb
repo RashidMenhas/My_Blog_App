1af2375585dc4abfb0dc4225c73ae3495d865a2b
Rashid Menhas <rashidmenhas10@gmail.com>
2023-03-03 13:17:54 +0500

    Fix linter errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,5 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name password email])
     devise_parameter_sanitizer.permit(:acount_update, keys: %i[name password email bio photo])
-end
+  end
 end
