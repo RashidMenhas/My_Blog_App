a56461c72135527d7516ab7c67c44e8195c5071f
Rashid Menhas <rashidmenhas10@gmail.com>
2023-03-05 11:04:29 +0500

    Class Api::V1 which is inherited from actioncontroller::Api and access token.

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -1,3 +1,12 @@
 class Api::V1::ApplicationController < ActionController::Api
+before_action :restrict_access
+respond_to :json
 
+private
+
+def restrict_access
+    api_key = ApiKey.find_by_access_token(params[:access_token])
+    head :unauthorized unless api_key
+
+end
 end
